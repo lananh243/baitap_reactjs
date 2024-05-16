e363861c2b19247cc4bb258880c23c458408ff17
lan anh234lan anh 234 <nguyenthilananh21092005@gmail.com>
2024-05-16 08:38:53 +0700

    bt session29

--- session30/src/Components/b7/Bai7.tsx
+++ session30/src/Components/b7/Bai7.tsx
@@ -1,23 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function Bai7() {
-    const [numbers, setNumbers] = useState<number[]>([]);
+  const [numbers, setNumbers] = useState<number[]>([]);
 
-    const handleAddElement = () => {
-        const random:number = Math.floor(Math.random() * 100) + 1;
-        setNumbers([...numbers, random]);
-    };
+  const handleAddElement = () => {
+    const random: number = Math.floor(Math.random() * 100) + 1;
+    setNumbers([...numbers, random]);
+  };
+
   return (
-    <div>Bài 7
-        <h2>Mảng số : <i>{JSON.stringify(numbers)}</i></h2>
-        <button onClick={handleAddElement}>Add element</button>
-        {numbers.length > 0 && (
-        <ul>
-          {numbers.map((number, index) => (
-            <li key={index}>{number}</li>
-          ))}
-        </ul>
-      )}
+    <div>
+      Bài 7
+      <h2>Mảng số: <i>{JSON.stringify(numbers)}</i></h2>
+      <button onClick={handleAddElement}>Add element</button>
     </div>
-  )
+  );
 }
