ba6acc6376406516cd85f22e7d431b6c9ef15ab0
lan anh234lan anh 234 <nguyenthilananh21092005@gmail.com>
2024-06-10 01:00:09 +0700

    Bài tập

---
+++ session55/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      {/* 
+        API : application programing interface
+        giúp cho các ứng dụng có thể giao tiếp với database , tương tác với cơ sở dữ liệu (server)
+
+       */}
+    </div>
+  )
+}
